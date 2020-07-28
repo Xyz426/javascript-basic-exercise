070d2fa2d1ca895879325bca7b7069d798b15993
xyz <xyzzhybz@163.com>
2020-07-28 15:54:18 +0800

    [yizuo.xiao]exercise_01 finished

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,12 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
+   number = number.toFixed(2);
+
+  if (option) {
+    number = `$ ${number}`;
+  }
+  return number;
 
   throw new Error('Please delete this line and implement the function');
 }
