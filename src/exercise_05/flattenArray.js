ea3b8ba4b29875df1844047b55cda428ba255407
xyz <xyzzhybz@163.com>
2020-07-28 19:20:50 +0800

    [yizuo.xiao]exercise_05 finished

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,22 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(array == null){
+    throw('Flatten undefined or null array');
+  }
+
+  const res = [];
+
+  for(let i = 0 ; i < array.length(); i++){
+    //说明是一维数组
+    if(array[i].length == 0){
+      res.push(array[i]);
+    }else{
+      for(let j = 0 ; j < array[i].length ;j++){
+        res.push(array[i][j]);
+      }
+    }
+  }
+
+  return res;
 }
