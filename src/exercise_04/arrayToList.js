dbecc12a8e58633f742002cc934c2fe5d491cccc
xyz <xyzzhybz@163.com>
2020-07-28 18:45:17 +0800

    [yizuo.xiao\]exercise_04 finished

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,19 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(array == null){
+    throw ('Creating list from undefined array');
+  }
+  if(array.length == 0){
+    throw('Creating list from empty array');
+  }
+
+  const list = {value : array[0], next : null};
+  let temp = list;
+  for(let i= 1 ; i < array.length ;i++){
+    temp.next = {value:array[i],next:null};
+    temp = temp.next;
+  }
+
+  return list;
 }
